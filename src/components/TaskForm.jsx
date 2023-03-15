f4ee7fddef8da7fd6c19ad38b5c70734589eccd0
TomasMadariaga <tomas.madariaga@municipiodetandil.edu.ar>
2023-03-15 14:36:33 -0300

    first commit

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,39 @@
+import { useState, useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
+
+export const TaskForm = () => {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const { createTask } = useContext(TaskContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    createTask({ title, desc });
+    setTitle("");
+    setDesc("");
+  };
+  return (
+    <div className="max-w-md mx-auto">
+      <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
+        <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea</h1>
+        <input
+          placeholder="Escribe tu tarea"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          value={title}
+          autoFocus
+        className="bg-slate-300 p-3 w-full mb-2"/>
+        <textarea
+          placeholder="Escribe la descripcion de la tarea"
+          onChange={(e) => {
+            setDesc(e.target.value);
+          }}
+          value={desc}
+          className="bg-slate-300 p-3 w-full mb-2"></textarea>
+        <button className="bg-indigo-500 px-3 py-1 text-white">Guardar</button>
+      </form>
+    </div>
+  );
+};
